78c59dcce9131998b2e66cc82961ddcfb81ab101
Csonka Bálint <balint.csonka@ctdi.eu>
2023-10-19 07:56:24 +0200

    reorganized controller

---
+++ src/ColorTemperatureController.mjs
@@ -0,0 +1,105 @@
+/**
+ * @typedef {import('dirigera').Light} Light
+ */
+import suncalc from 'suncalc'
+
+export default class ColorTemperatureController {
+	/**
+	 * @type {number}
+	 */
+	MAX_TEMP = 5500
+
+	/**
+	 * @type {number}
+	 */
+	MIN_TEMP = 2200
+
+	/**
+	 * @description The DIRIGERA client
+	 */
+	client
+
+	/**
+	 * @type {Set<string>}
+	 */
+	temporaryExclusion = new Set()
+
+	/**
+	 * @type {Set<string>}
+	 */
+	ignoreUpdate = new Set()
+
+	constructor (client) {
+		this.client = client
+		setInterval(this.update.bind(this), 0.1 * 60 * 1000)
+		this.update()
+	}
+
+	/**
+	 * @param {Array<Light>} lights The list of lights to update the color temperature of
+	 * @returns {Promise<any[]>} A promise that resolves if all lights has been set
+	 */
+	async _updateColorTemperature (lights) {
+		const hubStatus = await this.client.hub.status()
+
+		const now = new Date()
+		const latitude = hubStatus.attributes.coordinates.latitude
+		const longitude = hubStatus.attributes.coordinates.longitude
+		const sunPosition = suncalc.getPosition(now, latitude, longitude)
+
+		const fraction = sunPosition.altitude * 2.0 / Math.PI
+
+		const colorTemperature = Math.round((this.MIN_TEMP * 1.0) + (Math.max(fraction, 0) * (this.MAX_TEMP - this.MIN_TEMP)))
+
+		if (lights.length > 1) console.log(`Setting ${lights.length} lights to ${colorTemperature} K`)
+		else if (lights.length === 1) console.log(`Setting ${lights[0].attributes.customName} to ${colorTemperature} K`)
+
+		return Promise.all(lights.map(l => this.client.devices.setAttributes({
+			id: l.id,
+			attributes: {
+				colorTemperature
+			}
+		}).then(() => this.ignoreUpdate.add(l.id))))
+	}
+
+	/**
+	 * @returns {Promise<Array<Light>>} A list of color temperature capable lights
+	 */
+	async getOnColorTemperatureLights () {
+		const lights = await this.client.lights.list()
+		const colorTemperatureLights = lights.filter(l => l.capabilities.canReceive.indexOf('colorTemperature') > -1)
+		return colorTemperatureLights.filter(light => light.attributes.isOn)
+	}
+
+	async update () {
+		const onColorTemperatureLights = await this.getOnColorTemperatureLights()
+		const lightsToBeUpdated = onColorTemperatureLights.filter(l => !this.temporaryExclusion.has(l.id))
+		await this._updateColorTemperature(lightsToBeUpdated)
+	}
+
+	/**
+	 * @param {boolean} isOn True if the light was turned on, false if it was turned off
+	 * @param {Light} light The light that was controlled
+	 */
+	async onIsOnChanged (isOn, light) {
+		if (isOn) {
+			await this._updateColorTemperature([light])
+		} else {
+			this.temporaryExclusion.delete(light.id)
+		}
+	}
+
+	/**
+	 * @param {number} colorTemperature The color temperature value in Kelvin
+	 * @param {Light} light The light that was controlled
+	 */
+	onColorTemperatureChanged (colorTemperature, light) {
+		if (this.ignoreUpdate.has(light.id)) {
+			this.ignoreUpdate.delete(light.id)
+		} else {
+			if (!this.temporaryExclusion.has(light.id)) {
+				this.temporaryExclusion.add(light.id)
+			}
+		}
+	}
+}
--- src/index.mjs
+++ src/index.mjs
@@ -2,7 +2,7 @@
  * @typedef {import('dirigera').Light} Light
  */
 import { createDirigeraClient } from 'dirigera'
-import suncalc from 'suncalc'
+import ColorTemperatureController from './ColorTemperatureController.mjs'
 
 /**
  * @type {string}
@@ -10,109 +10,29 @@ import suncalc from 'suncalc'
 const DIRIGERA_TOKEN = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImI2NmY0MTMwOTYxZjliN2U4ZjcyMDdlOTM0MWRjYTVjY2RmODcyZjM2YTZlY2U0YTg1MjdkYTMzZTkzZTY3NDYifQ.eyJpc3MiOiI4MDU2Yjc5MS04MzY4LTRkNjEtYTNkZC04NmY4OTlhNGI2MTAiLCJ0eXBlIjoiYWNjZXNzIiwiYXVkIjoiaG9tZXNtYXJ0LmxvY2FsIiwic3ViIjoiMzY4ZDcwNzUtYzIxMC00ZWYwLThhMjYtNzM1NzE4ZDllMWNlIiwiaWF0IjoxNjk3NTMwNTAxLCJleHAiOjIwMTMxMDY1MDF9.M3xSFdMb_PloWvhw4ReBHwD7unP3wky2Ghg-jQME-HrisBX1Zfv7ydGLYfpyCfBJU_YaDuDFXKSVR2MGNLu4Gg'
 
 /**
- * @type {number}
- */
-const LATITUDE = 48.0567975201887
-
-/**
- * @type {number}
- */
-const LONGITUDE = 20.755862200951483
-
-/**
- * @type {number}
- */
-const MAX_TEMP = 5500
-
-/**
- * @type {number}
- */
-const MIN_TEMP = 2200
-
-/**
- * @type {Set<string>}
- */
-const ignoreUpdate = new Set()
-
-/**
  * Async wrapper
  */
 async function App () {
-  const client = await createDirigeraClient({ accessToken: DIRIGERA_TOKEN })
+	const client = await createDirigeraClient({ accessToken: DIRIGERA_TOKEN })
 
-  /**
-   * @returns {Promise<Array<Light>>} A list of color temperature capable lights
-   */
-  const getOnColorTemperatureLights = async function () {
-    const lights = await client.lights.list()
-    const colorTemperatureLights = lights.filter(l => l.capabilities.canReceive.indexOf('colorTemperature') > -1)
-    return colorTemperatureLights.filter(light => light.attributes.isOn)
-  }
+	const ctc = new ColorTemperatureController(client)
 
-  /**
-   * @param {Array<Light>} lights The list of lights to update the color temperature of
-   * @returns {Promise<any[]>} A promise that resolves if all lights has been set
-   */
-  const updateColorTemperature = function (lights) {
-    const now = new Date()
-    const sunPosition = suncalc.getPosition(now, LATITUDE, LONGITUDE)
-    const fraction = sunPosition.altitude * 2.0 / Math.PI
+	client.startListeningForUpdates(async (updateEvent) => {
+		if (updateEvent.data.type !== 'light') return
 
-    const colorTemp = Math.round((MIN_TEMP * 1.0) + (Math.max(fraction, 0) * (MAX_TEMP - MIN_TEMP)))
+		/**
+		 * @type {Light}
+		 */
+		const light = await client.lights.get({ id: updateEvent.data.id })
 
-    if (lights.length > 1) console.log(`Setting ${lights.length} lights to ${colorTemp} K`)
-    else if (lights.length === 1) console.log(`Setting ${lights[0].attributes.customName} to ${colorTemp} K`)
+		if (typeof updateEvent.data.attributes.isOn === 'boolean') {
+			await ctc.onIsOnChanged(updateEvent.data.attributes.isOn, light)
+		}
 
-    return Promise.all(lights.map(l => client.devices.setAttributes({
-      id: l.id,
-      attributes: {
-        colorTemperature: colorTemp
-      }
-    }).then(_ => {
-      ignoreUpdate.add(l.id)
-    })))
-  }
-
-  /**
-   * @type {Set<string>}
-   */
-  const temporaryExclusion = new Set()
-
-  const updateLambda = async () => {
-    const onColorTemperatureLights = await getOnColorTemperatureLights()
-    const lightsToBeUpdated = onColorTemperatureLights.filter(l => !temporaryExclusion.has(l.id))
-    updateColorTemperature(lightsToBeUpdated)
-  }
-
-  setInterval(updateLambda, 1 * 60 * 1000)
-  updateLambda()
-
-  client.startListeningForUpdates(async (updateEvent) => {
-    if (updateEvent.data.type !== 'light') return
-
-    /**
-     * @type {Light}
-     */
-    const light = await client.lights.get(updateEvent.data)
-
-    if (typeof updateEvent.data.attributes.isOn === 'boolean') {
-      if (updateEvent.data.attributes.isOn) {
-        updateColorTemperature([light])
-      } else {
-        temporaryExclusion.delete(light.id)
-      }
-    }
-
-    if (typeof updateEvent.data.attributes.colorTemperature !== 'undefined') {
-      if (ignoreUpdate.has(light.id)) {
-        ignoreUpdate.delete(light.id)
-      } else {
-        if (!temporaryExclusion.has(light.id)) {
-          temporaryExclusion.add(light.id)
-        }
-      }
-    }
-  })
+		if (typeof updateEvent.data.attributes.colorTemperature !== 'undefined') {
+			await ctc.onColorTemperatureChanged(updateEvent.data.attributes.colorTemperature, light)
+		}
+	})
 }
 
 App()
