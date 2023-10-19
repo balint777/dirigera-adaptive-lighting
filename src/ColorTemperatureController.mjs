a8d60304c5192c6efb8a4280c4f04df64e832396
Csonka Bálint <balint.csonka@ctdi.eu>
2023-10-19 20:02:44 +0200

    revert

--- src/ColorTemperatureController.mjs
+++ src/ColorTemperatureController.mjs
@@ -37,23 +37,12 @@ export default class ColorTemperatureController {
 		const longitude = hubStatus.attributes.coordinates.longitude
 		const sunPosition = suncalc.getPosition(now, latitude, longitude)
 
-		const altitude = sunPosition.altitude * 2.0 / Math.PI
+		const fraction = sunPosition.altitude * 2.0 / Math.PI
 
 		return Promise.all(lights.map(l => {
-			const horizon = 3500
-			const zenith = 5500
-
-			// const minTemp = l.attributes.colorTemperatureMax || 2200
-			// const maxTemp = l.attributes.colorTemperatureMin || 5500
-			/**
-			 * zenith    -| - - - - - - . - - - - - -
-			 *            |       .           .
-			 * altitude  -| -  🌞   - - - - - -  . -
-			 *            |  .                     .
-			 *            | .                       .
-			 * horizon    |. - - - - - - - - - - - - .
-			 */
-			const colorTemperature = Math.round((horizon * 1.0) + (altitude * (zenith - horizon)))
+			const minTemp = l.attributes.colorTemperatureMax || 2200
+			const maxTemp = l.attributes.colorTemperatureMin || 5500
+			const colorTemperature = Math.round((minTemp * 1.0) + (Math.max(fraction, 0) * (maxTemp - minTemp)))
 			console.info(`Setting ${l.attributes.customName} in the ${l.room.name} to ${colorTemperature} K`)
 
 			return this.client.devices.setAttributes({
